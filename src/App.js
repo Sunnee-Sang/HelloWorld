3502b5fcd827ee7d4592c8ac9044821170d95eff
Xiaoyue Sang <xiaoyuesang@XiaoyuematoAir.attlocal.net>
2021-02-24 21:01:53 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count:0
+    }
+  }
+  handleClick = (e) => {
+    let tem= this.state.count
+    this.setState({count: tem + 5})
+  }
+  
+  render() {
+    return (
+      <div className="App">
+       <div> {this.state.count} </div>
+       <button onClick={this.handleClick}>Click</button> 
+      </div>
+    );
+  }
 }
-
 export default App;
